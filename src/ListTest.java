29efc3bdb0851ba6e48fdbd85356f013cc578b05
LeoNielsen <leo.nielsen21@gmail.com>
2021-09-07 10:45:13 +0200

    test isEmpty in List Class

--- src/ListTest.java
+++ src/ListTest.java
@@ -1,15 +1,23 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListTest {
 
     List list = new List();
 
-    @org.junit.jupiter.api.Test
-    void insertFormHead() {
-        assertEquals("1", list.insertFormHead("1"));
-        assertEquals("2", list.insertFormHead("2"));
-        assertEquals("3", list.insertFormHead("3"));
+    @Test
+    void isEmpty() {
+        assertTrue(list.isEmpty());
+        list.insertFormHead("1");
+        assertFalse(list.isEmpty());
     }
 
+    @org.junit.jupiter.api.Test
+    void insertFormHead() {
+        assertEquals("1", list.insertFormHead("1").data);
+        assertEquals("2", list.insertFormHead("2").data);
+        assertEquals("3", list.insertFormHead("3").data);
+    }
 
 }
